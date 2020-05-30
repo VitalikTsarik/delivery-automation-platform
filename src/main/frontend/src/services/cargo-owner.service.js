fdb985628594ede53584aa2d49a22054bb73d2d4
VitalikTsarik <vitaliktsarik.forwork@gmail.com>
2020-05-30 18:56:09 +0300

    add CargoOwner service

---
+++ src/main/frontend/src/services/cargo-owner.service.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+
+const API_URL = "http://localhost:8080/api/cargo-owner/";
+
+class CargoOwnerService {
+  getContent() {
+    return axios.get(API_URL + "packages", {headers: authHeader()})
+      .then(response => response.data);
+  }
+
+  createPackage(item) {
+    return axios.post(API_URL + "package", {...item}, {headers: authHeader()});
+  }
+
+  getPackage(id) {
+    return axios.get(API_URL + `package/${id}`, {headers: authHeader()})
+      .then(response => response.data);
+  }
+
+  editPackage({id, ...item}) {
+    return axios.put(API_URL + `package/${id}`, {...item}, {headers: authHeader()});
+  }
+
+  removePackage(id) {
+    return axios.delete(API_URL + `package/${id}`, {headers: authHeader()});
+  }
+}
+
+export default new CargoOwnerService();
