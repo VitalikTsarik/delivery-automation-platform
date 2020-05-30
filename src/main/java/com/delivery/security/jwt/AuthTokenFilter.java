3a3ea7c73360af237b5fc2d84215a227076004d4
MisterOlrox <nikitakholdobin@yandex.ru>
2020-05-30 21:15:09 +0300

    Fix stacktrace

--- src/main/java/com/delivery/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/delivery/security/jwt/AuthTokenFilter.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -44,6 +45,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
+        } catch (UsernameNotFoundException ignored) {
         } catch (Exception e) {
             logger.error("Cannot set user authentication: ", e);
         }
