af75f7f9763cbc96d67ee709e3902d98d639c534
VitalikTsarik <vitaliktsarik.forwork@gmail.com>
2020-06-01 19:48:01 +0300

    split assignRoute and startTrip requests

--- src/main/frontend/src/services/transporter.service.js
+++ src/main/frontend/src/services/transporter.service.js
@@ -21,7 +21,6 @@ class TransporterService {
     return axios.delete(API_URL + `trip/package?packageId=${cargoId}&tripId=${tripId}`, {headers: authHeader()});
   }
 
-
   getTrips() {
     return axios.get(API_URL + "trips", {headers: authHeader()})
       .then(response => response.data);
@@ -31,8 +30,12 @@ class TransporterService {
     return axios.put(API_URL + `trip/current-city?order=${order}&tripId=${tripId}`, null, {headers: authHeader()});
   }
 
-  startTrip(id, route) {
-    return axios.post(API_URL + "trip/started", {id: id, route: route}, {headers: authHeader()});
+  assignRoute(route, tripId) {
+    return axios.put(API_URL + "trip/route", {route: route, tripId: tripId}, {headers: authHeader()});
+  }
+
+  startTrip(id) {
+    return axios.post(API_URL + `trip/started?tripId=${id}`, null, {headers: authHeader()});
   }
 }
 
